202b06a83545cd19bd66b68aa96fa38f7d74ede9
007lva <007.lva@gmail.com>
2015-02-03 23:10:12 +0100

    clean main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,1 @@
-"use strict";
 
-var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
-
-var a = 1;
-
-var Person = function Person() {
-  _classCallCheck(this, Person);
-};
-
-var sumar = function (a, b) {
-  return a + b;
-};
