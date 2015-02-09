b0ce2db5ac88317ca9a02f0ce58cbfffb4008966
lvargas <lvargas@rankia.com>
2015-02-09 10:59:57 +0100

    added gulp-6to5-package and code-example

---
+++ src/es6/main.js
@@ -0,0 +1,18 @@
+(() => {
+  var target;
+  $(() => {
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+          location.hostname === this.hostname) {
+        target = $(this.hash);
+        target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+        if (target.length) {
+          $('html,body').animate({
+              scrollTop: target.offset().top
+          }, 1500);
+          return false;
+        }
+      }
+    });
+  })
+}(jQuery))
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
+"use strict";
 
+(function () {
+  var target;
+  $(function () {
+    $("a[href*=#]:not([href=#])").click(function () {
+      if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+        target = $(this.hash);
+        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate({
+            scrollTop: target.offset().top
+          }, 1500);
+          return false;
+        }
+      }
+    });
+  });
+})(jQuery);
